8edbcba209613bbdea082db158bd00b6eeebd89f
awieckert <wiec1369@gmail.com>
2018-09-11 16:02:51 -0500

    Added the get all paymentTypes get request, need to test once the DB is created

---
+++ Controllers/PaymentTypeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace BangazonAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentTypeController : ControllerBase
+    {
+
+        private readonly IConfiguration _config;
+
+        public PaymentTypeController(IConfiguration config) {
+            _config = config;
+        }
+
+        public IDbConnection Connection {
+            get {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        // GET: api/PaymentType
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            string sql = "SELECT * FROM PaymentType";
+
+            using (IDbConnection conn = Connection) {
+                var paymentTypeQuery = await conn.QueryAsync(sql);
+                return Ok(paymentTypeQuery);
+            }
+            
+        }
+
+        // GET: api/PaymentType/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/PaymentType
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/PaymentType/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}

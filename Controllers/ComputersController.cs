9221ad2131347783a337ad24a52f86cb42fbe2fa
Evan Lusky <ewlusky@gmail.com>
2018-09-13 15:36:18 -0500

    Computer Controller ready for testing

---
+++ Controllers/ComputersController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace BangazonAPI.Models
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ComputersController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public ComputersController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public IDbConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        //    GET /Computers
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            string sql = "SELECT * FROM Computers ";
+
+            Console.WriteLine(sql);
+
+            using (IDbConnection conn = Connection)
+            {
+                IEnumerable<Computer> computers = await conn.QueryAsync<Computer>(sql);
+                return Ok(computers);
+            }
+        }
+
+        // GET /Computers/5
+        [HttpGet("{id}", Name = "GetComputers")]
+        public async Task<IActionResult> Get([FromRoute]int id)
+        {
+            string sql = $"SELECT * FROM Computers WHERE Id = {id}";
+
+            using (IDbConnection conn = Connection)
+            {
+                IEnumerable<Computer> computer = await conn.QueryAsync<Computer>(sql);
+                return Ok(computer);
+            }
+        }
+
+        //POST /Computers
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Computer computer)
+        {
+            string sql = $@"INSERT INTO Computers
+            (Model, PurchaseDate, DecomissionDate)
+            VALUES
+            ('{computer.Model}', '{computer.PurchaseDate}', '{computer.DecommisionDate}');
+            select MAX(Id) from Computers;";
+
+            Console.WriteLine(sql);
+            using (IDbConnection conn = Connection)
+            {
+                var computerId = (await conn.QueryAsync<int>(sql)).Single();
+                computer.Id = computerId;
+                return CreatedAtRoute("GetComputers", new { id = computerId }, computer);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> ChangeProduct(int id, [FromBody] Computer computer)
+        {
+            string sql = $@"
+            UPDATE Computers
+            SET Model = '{computer.Model}', PurchaseDate = '{computer.PurchaseDate}', DecomissionDate = '{computer.DecommisionDate}' 
+            WHERE Id = {id}";
+
+            try
+            {
+                using (IDbConnection conn = Connection)
+                {
+                    int rowsAffected = await conn.ExecuteAsync(sql);
+                    if (rowsAffected > 0)
+                    {
+                        return new StatusCodeResult(StatusCodes.Status204NoContent);
+                    }
+                    throw new Exception("No rows affected");
+                }
+            }
+            catch (Exception)
+            {
+                if (!ProductExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        // DELETE /Computers/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            string sql = $@"DELETE FROM Computers WHERE Id = {id}";
+
+            using (IDbConnection conn = Connection)
+            {
+                int rowsAffected = await conn.ExecuteAsync(sql);
+                if (rowsAffected > 0)
+                {
+                    return new StatusCodeResult(StatusCodes.Status204NoContent);
+                }
+                throw new Exception("No rows affected");
+            }
+
+        }
+
+        private bool ProductExists(int id)
+        {
+            string sql = $"SELECT Id FROM Computers WHERE Id = {id}";
+            using (IDbConnection conn = Connection)
+            {
+                return conn.Query<Computer>(sql).Count() > 0;
+            }
+        }
+    }
+}

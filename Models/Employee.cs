c19f8f8c06aba107784fc7ad713d94ce04e93eff
Evan Lusky <ewlusky@gmail.com>
2018-09-13 10:58:28 -0500

    forgot to add

---
+++ Models/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonAPI.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime HireDate { get; set; }
+
+        public bool IsSupervisor { get; set; }
+
+        public int DepartmentId { get; set; }
+
+    }
+}
